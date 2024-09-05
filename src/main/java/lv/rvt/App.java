cacfad33e532699a75a0c3757e93da4425863882
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-09-05 09:51:37 +0000

    uZDEVUMS 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,20 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("jggj");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("ievadisavu vardu>>");
+        String name = scanner.nextLine();
+        System.out.print("ievadisavu uzvardu>>");
+        String lastname = scanner.nextLine();
+        System.out.print("ievadisavu gruppu>>");
+        String group = scanner.nextLine();-
+
+        System.out.println(name + " " + lastname + " " + group);
+
     }
 
 }
