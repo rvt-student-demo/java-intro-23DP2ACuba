69e53c2b42fd4ba871efa5b9ac94d7a2ccea8f25
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-09-11 09:33:40 +0000

    Uzdvums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,24 +1,24 @@
 package lv.rvt;
-import java.util.Scanner;
+import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Chicken:");
-        String chicken = scanner.nextLine();
-        System.out.println("Bacon (kg):");
-        String bacon = scanner.nextLine();
-        System.out.println("Tractor:");
-        String tractor = scanner.nextLine();
 
-
-        System.out.println("And finally, a summary:");
-        System.out.println(chicken);
-        System.out.println(bacon);
-        System.out.println(tractor);
-
+    // String userinput = scanner.nextLine();
+    // System.out.println(userinput);
+    // String start = "My name is ";
+    // String end = ", James Bond";
+    // int digit = 99;
+    // System.out.println("mani euro" + digit + 100);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ievadi savu vārdu:");
+    String name = scanner.nextLine();
+    System.out.println("Ievadi savu uzvārdu:");
+    String lastname = scanner.nextLine();
+    System.out.println("Ievadi savu gruppu:");
+    String group = scanner.nextLine();
+    System.out.println(name + " " + lastname + " " + group);
     }
 
 }
