a4bef8b740a0ed9de644a446c449f0db5f6b7042
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-09-12 09:31:43 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,19 +4,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Scanner scanner = new Scanner(System.in);
-        // String input = scanner.nextLine();
-        // int InputAsInt = Integer.valueOf(scanner.nextLine());
-        // int InputAsDouble = Double.valueOf(scanner.nextLine());
-        // System.out.println(InputAsInt * 10);
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give the first number:");
         int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the first number:");
         int second = Integer.valueOf(scanner.nextLine());
         System.out.println(first + "+" + second + "=" + (first + second));
-
+        System.out.println(first + "-" + second + "=" + (first - second));
+        System.out.println(first + "*" + second + "=" + (first * second));
+        System.out.println(first + "/" + second + "=" + (1.0 * first / second));
     }
 
 }
