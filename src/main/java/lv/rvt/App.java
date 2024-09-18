925b6349a105197d270474f052e8ece3cf55609a
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-09-18 08:21:21 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,15 +4,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the first number:");
-        int second = Integer.valueOf(scanner.nextLine());
-        System.out.println(first + "+" + second + "=" + (first + second));
-        System.out.println(first + "-" + second + "=" + (first - second));
-        System.out.println(first + "*" + second + "=" + (first * second));
-        System.out.println(first + "/" + second + "=" + (1.0 * first / second));
-    }
-
+            int number = 90;
+              
+                if (number > 0 && number < 50) {
+                System.out.println("failed");
+              } else if (number > 49 && number < 60) {
+                System.out.println("1");
+              } else if (number > 59 && number < 70) {
+                System.out.println("2");
+              } else if (number > 69 && number < 80) {
+                System.out.println("3");
+              } else if (number > 79 && number < 90) {
+                System.out.println("4");
+              } else if (number > 89) {
+                System.out.println("5");
+              } else {
+                System.out.println("Impossible");
+              }
+        }
+        
 }
