ee32b04e3140879035b668548605cb6f94b2a5e3
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-09-19 09:20:41 +0000

    Square root of sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,27 +5,12 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        double value = Double.valueOf(scanner.nextLine());
         
-              
-            if (value >= 5000 && value < 25000) {
-                System.out.println(100 + (value-5000) * 0.08);
-
-              } else if (value >= 25000 && value < 55000) {
-                System.out.println(1700 + (value-25000) * 0.1);
-
-              } else if (value >= 55000 && value < 200000) {
-                System.out.println(4700 + (value-55000) * 0.12);
-
-              } else if (value >= 200000 && value < 1000000) {
-                System.out.println(22100 + (value-200000) * 0.15);
-
-              } else if (value >= 1000000) {
-                System.out.println(142100 + (value-1000000) * 0.17);
-                
-              } else {
-                System.out.println("No tax");
-              }
-
+        System.out.println("Value 1");
+        int value1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Value 1");
+        int value2 = Integer.valueOf(scanner.nextLine());
+        double result = Math.sqrt(value1 + value2);
+        System.out.println(result);
     }
 }
