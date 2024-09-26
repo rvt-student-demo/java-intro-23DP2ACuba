e73c09ab00b8a8f91989e196e39f97a1c6c41268
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-09-26 09:26:14 +0000

    Factorial

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,13 +4,15 @@ public class App
 {
     public static void main( String[] args )
     {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a number:");
-        int start = Integer.valueOf(scanner.nextLine());
-        for (int i = start; i < 101; i++) {
-            System.out.println(i);
+        int end = Integer.valueOf(scanner.nextLine());
+        int f = 1;
+        for (int i = 1; i <= end; i++) {
+            f *= i;
         }
-
+        System.out.println(f);
             
     }
 }
