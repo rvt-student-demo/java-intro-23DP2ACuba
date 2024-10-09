8f6d7476ca8745dec33755220f9af0c06f792152
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-10-09 09:20:53 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,6 +4,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stars.printStars(4);
+        Stars.printStars(5);
+        Stars.printStars(3);
+        Stars.printStars(11);
+        System.out.println();
+        Stars.printSquare(4);
+        System.out.println();
+        Stars.printRectangle(4, 16);
+        System.out.println();
+        Stars.printTriangle(4);
+
     }
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -3,6 +3,32 @@ package lv.rvt;
 public class Stars {
     public static void printStars(int h){
         for (int i = 0; i < h; i++) {
+            System.out.print("*");
+        }             
+        System.out.println();
+
+
+    }
+    public static void printSquare(int h){
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < h; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+
+    }
+    public static void printRectangle(int h, int w){
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+
+    }
+    public static void printTriangle(int h){
+        for (int i = 0; i < h; i++) {
             for (int j = 0; j < i+1; j++){
                 System.out.print("*");
             }
