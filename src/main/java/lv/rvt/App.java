17172e985313402f13d70c523006389d0d98dff0
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-10-09 09:06:45 +0000

    Printing stars part 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,17 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many times?");
-        int input = Integer.valueOf(scanner.nextLine());
-        printUntilNumber(input);
-
-    }
-    public static void printUntilNumber(int input) {
-        while (input >= 0) {
-            System.out.println("In a hole in the ground there lived a method");
-            input--;
-    }
+        Stars.printStars(5);
+        Stars.printStars(3);
+        Stars.printStars(11);
     }
 }
---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,10 @@
+package lv.rvt;
+
+public class Stars {
+    public static void printStars(int n){
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        } 
+        System.out.println();
+    }
+}
