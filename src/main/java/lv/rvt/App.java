e8286439ae2c7a67209ffbad51d963a13cc772c6
23DP2ACuba <166109704+23DP2ACuba@users.noreply.github.com>
2024-10-09 09:08:29 +0000

    Printing a square part 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,8 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Stars.printStars(5);
-        Stars.printStars(3);
-        Stars.printStars(11);
+        Stars.printStars(4);
     }
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -3,8 +3,11 @@ package lv.rvt;
 public class Stars {
     public static void printStars(int n){
         for (int i = 0; i < n; i++) {
-            System.out.print("*");
+            for (int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         } 
-        System.out.println();
+
     }
 }
